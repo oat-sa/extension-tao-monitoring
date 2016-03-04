c0c56d91248b388c8b4ccab87dfa3eae84892e27
Alexander Zagovorichev <zagovorichev@1pt.com>
2016-03-04 18:01:03 +0300

    Switch off uninstall script for tetst taker delivery log

--- manifest.php
+++ manifest.php
@@ -40,7 +40,9 @@ return array(
     ),
     'uninstall' => array(
         'php' => array(
-            __DIR__.DIRECTORY_SEPARATOR.'scripts'.DIRECTORY_SEPARATOR.'uninstall'.DIRECTORY_SEPARATOR.'RdsTestTakerDeliveryLog.php',
+            //
+			//doesn't work for now
+            //__DIR__.DIRECTORY_SEPARATOR.'scripts'.DIRECTORY_SEPARATOR.'uninstall'.DIRECTORY_SEPARATOR.'RdsTestTakerDeliveryLog.php',
             //'oat\\taoMonitoring\\scripts\\uninstall\\Delivery\\TestTakerLog',
         )
     ),
