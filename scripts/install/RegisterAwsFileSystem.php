c4f115d408f1a7614c1734631a14b0c47df1f4a3
Gyula Szucs <gyula@taotesting.com>
2016-12-16 14:49:07 +0100

    AWS fly system registration modified.

--- scripts/install/RegisterAwsFileSystem.php
+++ scripts/install/RegisterAwsFileSystem.php
@@ -15,27 +15,8 @@ class RegisterAwsFileSystem extends \common_ext_action_InstallAction
     public function __invoke($params)
     {
         $serviceManager = $this->getServiceManager();
-
-        try{
-            $service = $serviceManager->get(FileSystemService::SERVICE_ID);
-            $options = $service->getOptions();
-        } catch (ServiceNotFoundException $e) {
-            //do nothing
-        }
-
-        $options[FileSystemService::OPTION_ADAPTERS]['taoAwsS3'] = [
-            'class'   => 'oat\\awsTools\\AwsFlyWrapper',
-            'options' => [
-                [
-                    'cache'  => true,
-                    'bucket' => '',
-                    'client' => 'generis/awsClient',
-                    'prefix' => 'tao'
-                ]
-            ]
-        ];
-
-        $newService = new AwsFileSystemService($options);
-        $serviceManager->register(FileSystemService::SERVICE_ID, $newService);
+        $service = $serviceManager->get(FileSystemService::SERVICE_ID);
+        $service->createFileSystem('taoMonitoring');
+        $serviceManager->register(FileSystemService::SERVICE_ID, $service);
     }
 }
