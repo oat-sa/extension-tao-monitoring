330755deeb0ea0346b7f59e6b88314a2a239a900
Oleksander Zagovorychev <zagovorichev@gmail.com>
2018-10-29 13:09:36 +0300

    New MonitoringPlugService

---
+++ config/header/MonitoringPlugService.conf.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * # No monitoring by default to exclude extra loading on the storage
+ * # each installation needs to provide its own list of monitoring
+ *
+ * \oat\taoMonitoring\model\DeliveryLog\DeliveryLogService::SERVICE_ID,
+ * \oat\taoMonitoring\model\TestTakerDeliveryActivityLogInterface::SERVICE_ID,
+ */
